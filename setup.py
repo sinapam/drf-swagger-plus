f51654924d8bae9d4bedd5d06713daadf9706102
sinapam <sinapam@proteus-tech.com>
2016-07-16 21:43:29 +0700

    change version to 0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='drf-swagger-plus',
-    version='0.3',
+    version='0.4',
     description='An extension for django-rest-swagger',
     author='Paul and Betty Devs',
     author_email='info@paulandbetty.asia',
